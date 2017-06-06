2806b31baf183e8023c77276246c3e0d2301ac8e
taxpayer131 <chris.stefan1844@gmail.com>
2017-06-06 14:11:58 -0700

    finished all five JS questions

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 'use strict';
+
+// first question
 var child = prompt('Does Chris have children?').toUpperCase();
   if (child === 'YES' || child === 'Y') {
     alert("Correct!");
@@ -6,3 +8,39 @@ var child = prompt('Does Chris have children?').toUpperCase();
     else {
     alert('Incorrect!  Chris has one son.');
   }
+
+// second question
+  var marriage = prompt('Is Chris married?').toUpperCase();
+    if (marriage === 'YES' || marriage === 'Y') {
+      alert("Correct!");
+    }
+      else {
+      alert('Incorrect!  Chris is married.');
+    }
+
+// third question
+    var dev = prompt('Does Chris want to be a developer?').toUpperCase();
+      if (dev === 'YES' || dev === 'Y') {
+        alert("Correct!");
+      }
+        else {
+        alert('Incorrect!  Chris is re-training himself to become a developer.');
+      }
+
+//fourth question
+      var it = prompt('Does Chris have experience in information technology (IT)?').toUpperCase();
+        if (it === 'YES' || it === 'Y') {
+          alert("Correct!");
+        }
+          else {
+          alert('Incorrect!  Chris has has extensive experience in IT.');
+        }
+
+// fifth question
+        var car = prompt('Does Chris own a car?').toUpperCase();
+          if (car === 'NO' || car === 'N') {
+            alert("Correct!");
+          }
+            else {
+            alert('Incorrect!  Chris does not own a car.');
+          }
