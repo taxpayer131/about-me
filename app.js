e5a1976b31604ced5593334887bfd010cb93d474
taxpayer131 <chris.stefan1844@gmail.com>
2017-06-06 14:26:05 -0700

    fixed console.log messages

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 // first question
 var child = prompt('Does Chris have children?').toUpperCase();
   if (child === 'YES' || child === 'Y') {
@@ -8,6 +9,7 @@ var child = prompt('Does Chris have children?').toUpperCase();
     else {
     alert('Incorrect!  Chris has one son.');
   }
+console.log("Answer to the question of having children is " + child);
 
 // second question
   var marriage = prompt('Is Chris married?').toUpperCase();
@@ -17,6 +19,7 @@ var child = prompt('Does Chris have children?').toUpperCase();
       else {
       alert('Incorrect!  Chris is married.');
     }
+console.log("Answer to the question or being married or not is " + marriage);
 
 // third question
     var dev = prompt('Does Chris want to be a developer?').toUpperCase();
@@ -26,6 +29,7 @@ var child = prompt('Does Chris have children?').toUpperCase();
         else {
         alert('Incorrect!  Chris is re-training himself to become a developer.');
       }
+console.log("Answer to the question of Chris wanting to be a developer is " + dev);
 
 //fourth question
       var it = prompt('Does Chris have experience in information technology (IT)?').toUpperCase();
@@ -35,6 +39,7 @@ var child = prompt('Does Chris have children?').toUpperCase();
           else {
           alert('Incorrect!  Chris has has extensive experience in IT.');
         }
+console.log("Answer to the question of whether or not Chris has IT exp is " + it);
 
 // fifth question
         var car = prompt('Does Chris own a car?').toUpperCase();
@@ -44,3 +49,4 @@ var child = prompt('Does Chris have children?').toUpperCase();
             else {
             alert('Incorrect!  Chris does not own a car.');
           }
+console.log("Answer to the question of Chris owning a car is " + car);
