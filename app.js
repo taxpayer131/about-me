cbf78e9b0da3b826b9a33b50ce041ff18ad5bf8d
taxpayer131 <chris.stefan1844@gmail.com>
2017-06-08 09:55:31 -0700

    app.js update.  trying to fix scoreKeeper.

--- app.js
+++ app.js
@@ -1,82 +1,86 @@
-// 'use strict';
-//
-// //
-// // first question
-// var child = prompt('Does Chris have children?').toUpperCase();
-// if (child === 'YES' || child === 'Y') {
-//   alert("Correct!");
-// }
-// else {
-//   alert('Incorrect!  Chris has one son.');
-// }
-// console.log("Answer to the question of having children is " + child);
-//
-// // second question
-// var marriage = prompt('Is Chris married?').toUpperCase();
-// if (marriage === 'YES' || marriage === 'Y') {
-//   alert("Correct!");
-// }
-// else {
-//   alert('Incorrect!  Chris is married.');
-// }
-// console.log("Answer to the question or being married or not is " + marriage);
-//
-// // third question
-// var dev = prompt('Does Chris want to be a developer?').toUpperCase();
-// if (dev === 'YES' || dev === 'Y') {
-//   alert("Correct!");
-// }
-// else {
-//   alert('Incorrect!  Chris is re-training himself to become a developer.');
-// }
-// console.log("Answer to the question of Chris wanting to be a developer is " + dev);
-//
+'use strict';
+
+var scoreKeeper = 0; //Variable set for the scorekeeper on questions 1-7.
+// first question
+var child = prompt('Does Chris have children?').toUpperCase();
+if (child === 'YES' || child === 'Y') {
+  alert('Correct!');
+  scoreKeeper++;
+}
+else {
+  alert('Incorrect!  Chris has one son.');
+}
+console.log('Answer to the question of having children is ' + child);
+
+// second question
+var marriage = prompt('Is Chris married?').toUpperCase();
+if (marriage === 'YES' || marriage === 'Y') {
+  alert('Correct!');
+  scoreKeeper++;
+}
+else {
+  alert('Incorrect!  Chris is married.');
+}
+console.log('Answer to the question or being married or not is ' + marriage);
+
+// third question
+var dev = prompt('Does Chris want to be a developer?').toUpperCase();
+if (dev === 'YES' || dev === 'Y') {
+  alert('Correct!');
+  scoreKeeper++;
+}
+else {
+  alert('Incorrect!  Chris is re-training himself to become a developer.');
+}
+console.log('Answer to the question of Chris wanting to be a developer is ' + dev);
+
 // //fourth question
-// var it = prompt('Does Chris have experience in information technology (IT)?').toUpperCase();
-// if (it === 'YES' || it === 'Y') {
-//   alert("Correct!");
-// }
-// else {
-//   alert('Incorrect!  Chris has has extensive experience in IT.');
-// }
-// console.log("Answer to the question of whether or not Chris has IT exp is " + it);
-//
-// // fifth question
-// var car = prompt('Does Chris own a car?').toUpperCase();
-// if (car === 'NO' || car === 'N') {
-//   alert('Correct!');
-// }
-// else {
-//   alert('Incorrect!  Chris does not own a car.');
-// }
-// console.log('Answer to the question of Chris owning a car is ' + car);
-//
+var it = prompt('Does Chris have experience in information technology (IT)?').toUpperCase();
+if (it === 'YES' || it === 'Y') {
+  alert('Correct!');
+  scoreKeeper++;
+}
+else {
+  alert('Incorrect!  Chris has has extensive experience in IT.');
+}
+console.log('Answer to the question of whether or not Chris has IT exp is ' + it);
+
+//fifth question
+var car = prompt('Does Chris own a car?').toUpperCase();
+if (car === 'NO' || car === 'N') {
+  alert('Correct!');
+  scoreKeeper++;
+}
+else {
+  alert('Incorrect!  Chris does not own a car.');
+}
+console.log('Answer to the question of Chris owning a car is ' + car);
+
 //Sixth question
-// var guess;
-// var counter = 1;
-// var correctNum = 4;
-//
-// while (number !== correctNum && counter <= 4) {
-//   number = parseInt(prompt('What is Chris\'s favorite number between 0 and 20?'));
-//   console.log('number guess', number);
-//   console.log('counter', counter);
-//
-//   if (number < correctNum) {
-//     alert('Too low');
-//     counter++;
-//   } else if (number > correctNum) {
-//     alert('Too high');
-//     counter++;
-//   } else {
-//     alert('That\'s right! You guessed it in ' + counter + ' tries.');
-//     rightAnswers++;
-//     break;
-//   }
-// }
-// console.log('counter', counter);
-// if (counter > 4) {
-//   alert('Sorry! His favorite number is ' + correctNum);
-// }
+var guess;
+var counter = 1;
+var correctNum = 4;
+number = parseInt(prompt('What is Chris\'s favorite number between 0 and 20?'));
+while (number !== correctNum && counter <= 4) {
+  console.log('number guess', number);
+  console.log('counter', counter);
+
+  if (number < correctNum) {
+    alert('Too low');
+    counter++;
+  } else if (number > correctNum) {
+    alert('Too high');
+    counter++;
+  } else {
+    alert('That\'s right! You guessed it in ' + counter + ' tries.');
+    scoreKeeper++;
+    break;
+  }
+}
+console.log('counter', counter);
+if (counter > 4) {
+  alert('Sorry! His favorite number is ' + correctNum);
+}
 //Seventh question
 var countries = ['india', 'belgium', 'france'];
 var guessesLeft = 6;
@@ -87,6 +91,7 @@ while (guessesLeft > 0) {
   for (var i = 0; i < countries.length; i++) {
     if (ans === countries[i]) {
       alert('Correct!');
+      scoreKeeper++;
       guessesLeft = 0;
       break;
     }
@@ -100,3 +105,5 @@ while (guessesLeft > 0) {
     alert('No, guess again. You have ' + guessesLeft + ' guesses left.');
   }
 }
+//scorekeeper code
+alert('You got ' + scoreKeeper + ' out of 7 Correct.');
